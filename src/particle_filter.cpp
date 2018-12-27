edd2691f73b23358c3a976fa55a66b6757be0e28
Clark Li <clark.li86@gmail.com>
2018-12-27 03:00:30 -0800

    Reduce the number of particles to 200
    This passes the test.
    
    TODO: When the number is 100, sometimes it lost track of position due to
    calculation error. (NAN in best particle weight)

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,7 +30,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1.
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-  num_particles = 1000;  // TODO: Set the number of particles
+  num_particles = 200;  // TODO: Set the number of particles
 
   particles.resize(num_particles);
 
